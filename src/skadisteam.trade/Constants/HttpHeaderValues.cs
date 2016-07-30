e35cab8e86912acc6f06ade1232e3e521ec4490e
igeligel <kevinigeligeligel@gmail.com>
2016-07-30 22:48:06 +0200

    feat(models)
    add model for http header values

---
+++ src/skadisteam.trade/Constants/HttpHeaderValues.cs
@@ -0,0 +1,12 @@
+namespace skadisteam.trade.Constants
+{
+    internal static class HttpHeaderValues
+    {
+        internal const string AcceptHtmlImagesAndXml = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+        internal const string AcceptLanguageEnglish = "en-US;q=0.6,en;q=0.4,it;q=0.2";
+        internal const string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36";
+        internal const string GzipDeflateOrSdch = "gzip, deflate, sdch";
+        internal const string NoCache = "no-cache";
+        
+    }
+}
