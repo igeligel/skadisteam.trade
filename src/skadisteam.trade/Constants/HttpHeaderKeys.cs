79d8ba36df502eb422c18860fe7f6a261257aef1
igeligel <kevinigeligeligel@gmail.com>
2016-07-30 22:24:45 +0200

    feat(models)
    add model for http header keys

---
+++ src/skadisteam.trade/Constants/HttpHeaderKeys.cs
@@ -0,0 +1,14 @@
+namespace skadisteam.trade.Constants
+{
+    internal static class HttpHeaderKeys
+    {
+        internal const string Accept = "Accept";
+        internal const string AcceptEncoding = "Accept-Encoding";
+        internal const string AcceptLanguage = "Accept-Language";
+        internal const string CacheControl = "Cache-Control";
+        internal const string Pragma = "Pragma";
+        internal const string Referer = "Referer";
+        internal const string UpgradeInsecureRequest = "Upgrade-Insecure-Requests";
+        internal const string UserAgent = "User-Agent";
+    }
+}
