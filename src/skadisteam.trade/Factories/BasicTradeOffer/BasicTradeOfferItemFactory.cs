88ce413a12ae6eba0a0722b0c4128d6ef3efaba9
igeligel <kevinigeligeligel@gmail.com>
2016-08-03 19:36:56 +0200

    feat(factories)
    add factory for trade offer items

---
+++ src/skadisteam.trade/Factories/BasicTradeOffer/BasicTradeOfferItemFactory.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using AngleSharp.Dom;
+using skadisteam.trade.Models;
+
+namespace skadisteam.trade.Factories.BasicTradeOffer
+{
+    internal static class BasicTradeOfferItemFactory
+    {
+        internal static BasicTradeOfferItem Create(IElement angleSharpElement)
+        {
+            var basicTradeOfferItem = new BasicTradeOfferItem();
+            var dataEconomyItem =
+                angleSharpElement.Attributes.FirstOrDefault(
+                    e => e.Name == "data-economy-item").Value;
+            basicTradeOfferItem.DataEconomy =
+                DataEconomyFactory.GetEconomy(dataEconomyItem);
+            basicTradeOfferItem.ItemPicture =
+                angleSharpElement.Children.FirstOrDefault(
+                    e => e.TagName == "IMG")
+                    .Attributes.FirstOrDefault(e => e.Name == "src")
+                    .Value;
+            return basicTradeOfferItem;
+        }
+    }
+}
