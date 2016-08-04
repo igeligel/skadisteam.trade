d9e51fba5949eb04e0be3ace24fc0d9fa83003a8
igeligel <kevinigeligeligel@gmail.com>
2016-08-04 17:36:02 +0200

    feat(factories)
    add factory for a basic tradeoffer item list

---
+++ src/skadisteam.trade/Factories/BasicTradeOffer/BasicTradeOfferItemListFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using AngleSharp.Dom;
+using skadisteam.trade.Models;
+using System.Linq;
+
+namespace skadisteam.trade.Factories.BasicTradeOffer
+{
+    internal static class BasicTradeOfferItemListFactory
+    {
+        internal static List<BasicTradeOfferItem> CreateProfileItems(IHtmlCollection<IElement> tradeOfferItems)
+        {
+            var partnerItems = tradeOfferItems.LastOrDefault();
+            var partnerItemInstances =
+                partnerItems.QuerySelectorAll(".trade_item");
+            return partnerItemInstances.Select(BasicTradeOfferItemFactory.Create).ToList();
+        }
+
+        internal static List<BasicTradeOfferItem> CreatePartnerItems(IHtmlCollection<IElement> tradeOfferItems)
+        {
+            var partnerItems = tradeOfferItems.FirstOrDefault();
+            var partnerItemInstances =
+                partnerItems.QuerySelectorAll(".trade_item");
+            return partnerItemInstances.Select(BasicTradeOfferItemFactory.Create).ToList();
+        }
+    }
+}
