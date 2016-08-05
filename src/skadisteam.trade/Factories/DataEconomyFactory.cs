95812bf41e6d0d02ddc0b4bfbb08e0e94c8b81f3
igeligel <kevinigeligeligel@gmail.com>
2016-08-05 21:50:18 +0200

    refactor(factories)
    use new constants in the class and change the else block

--- src/skadisteam.trade/Factories/DataEconomyFactory.cs
+++ src/skadisteam.trade/Factories/DataEconomyFactory.cs
@@ -1,3 +1,4 @@
+using skadisteam.trade.Constants;
 using skadisteam.trade.Interfaces;
 using skadisteam.trade.Models.DataEconomy;
 
@@ -7,26 +8,31 @@ namespace skadisteam.trade.Factories
     {
         internal static IDataEconomy GetEconomy(string dataEconomyItem)
         {
-
-            if (dataEconomyItem.Contains("classinfo"))
+            if (dataEconomyItem.Contains(RegexPatterns.ClassInfo))
             {
                 return new PrivateDataEconomy
                 {
-                    AppId = int.Parse(dataEconomyItem.Split('/')[1]),
-                    ClassId = long.Parse(dataEconomyItem.Split('/')[2]),
-                    InstanceId = long.Parse(dataEconomyItem.Split('/')[3])
+                    AppId =
+                        int.Parse(dataEconomyItem.Split(Characters.BackSlash)[1]),
+                    ClassId =
+                        long.Parse(
+                            dataEconomyItem.Split(Characters.BackSlash)[2]),
+                    InstanceId =
+                        long.Parse(
+                            dataEconomyItem.Split(Characters.BackSlash)[3])
                 };
             }
-            else
+            return new PublicDataEconomy
             {
-                return new PublicDataEconomy
-                {
-                    AppId = int.Parse(dataEconomyItem.Split('/')[0]),
-                    ContextId = int.Parse(dataEconomyItem.Split('/')[1]),
-                    AssetId = long.Parse(dataEconomyItem.Split('/')[2]),
-                    SteamCommunityId = long.Parse(dataEconomyItem.Split('/')[3])
-                };
-            }
+                AppId =
+                    int.Parse(dataEconomyItem.Split(Characters.BackSlash)[0]),
+                ContextId =
+                    int.Parse(dataEconomyItem.Split(Characters.BackSlash)[1]),
+                AssetId =
+                    long.Parse(dataEconomyItem.Split(Characters.BackSlash)[2]),
+                SteamCommunityId =
+                    long.Parse(dataEconomyItem.Split(Characters.BackSlash)[3])
+            };
         }
     }
 }
