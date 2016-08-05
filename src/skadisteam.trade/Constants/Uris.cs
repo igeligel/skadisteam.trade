58f55b2ff104bbf0c27f2fcae8ae95c1df20ea5c
igeligel <kevinigeligeligel@gmail.com>
2016-08-05 22:00:53 +0200

    feat(constants)
    add constants for uris

---
+++ src/skadisteam.trade/Constants/Uris.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace skadisteam.trade.Constants
+{
+    internal static class Uris
+    {
+        internal static Uri SteamCommunityBase = new Uri(Urls.SteamCommunityBase);
+    }
+}
