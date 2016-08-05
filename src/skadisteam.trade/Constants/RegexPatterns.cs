ea0779a45bb3e2f325e6926e22f0697306adb541
igeligel <kevinigeligeligel@gmail.com>
2016-08-05 21:49:46 +0200

    feat(constants)
    add constant to the regex patterns

--- src/skadisteam.trade/Constants/RegexPatterns.cs
+++ src/skadisteam.trade/Constants/RegexPatterns.cs
@@ -2,6 +2,7 @@ namespace skadisteam.trade.Constants
 {
     internal static class RegexPatterns
     {
+        internal const string ClassInfo = "classinfo";
         internal const string CommunityId = "javascript\\SReportTradeScam\\S '";
         internal const string NewLine = "\n";
         internal const string SingleQuote = "'";
