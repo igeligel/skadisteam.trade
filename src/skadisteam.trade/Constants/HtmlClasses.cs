e17c4a8c1d2d1a7d77f37cb43a58f674f731f691
igeligel <kevinigeligeligel@gmail.com>
2016-08-06 19:15:20 +0200

    feat(constants)
    add more constants for html classes

--- src/skadisteam.trade/Constants/HtmlClasses.cs
+++ src/skadisteam.trade/Constants/HtmlClasses.cs
@@ -2,9 +2,11 @@ namespace skadisteam.trade.Constants
 {
     internal static class HtmlClasses
     {
+        internal const string Active = "active";
         internal const string InGame = "in-game";
         internal const string Offline = "offline";
         internal const string Online = "online";
         internal const string PlayerAvatar = "playerAvatar";
+        internal const string TradeOfferItemsCount = "tradeoffer_items_ctn";
     }
 }
