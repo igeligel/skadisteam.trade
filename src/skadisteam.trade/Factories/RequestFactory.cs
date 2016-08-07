12172194178bb64d4862264156265dfa65a45ef6
igeligel <kevinigeligeligel@gmail.com>
2016-08-07 23:15:39 +0200

    feat(factories)
    add method for creating requests to actual offer

--- src/skadisteam.trade/Factories/RequestFactory.cs
+++ src/skadisteam.trade/Factories/RequestFactory.cs
@@ -39,5 +39,27 @@ namespace skadisteam.trade.Factories
             }
             return response;
         }
+
+        internal static HttpResponseMessage GetTradeOfferResponseMessage(
+            HttpClientHandler httpClientHandler, string path, long steamCommunityId)
+        {
+            // https://steamcommunity.com
+
+            HttpResponseMessage response;
+            using (var client = new HttpClient(httpClientHandler))
+            {
+                client.BaseAddress = new Uri("https://steamcommunity.com");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.Accept, "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.AcceptEncoding, "gzip, deflate, sdch, br");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.AcceptEncoding, "de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4,it;q=0.2");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.UserAgent, "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.Referer, "http://steamcommunity.com/profiles/" + steamCommunityId + "/tradeoffers/");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.UpgradeInsecureRequest, "1");
+                client.DefaultRequestHeaders.Host =
+                    new Uri("https://steamcommunity.com").Host;
+                response = client.GetAsync(path).Result;
+            }
+            return response;
+        }
     }
 }
