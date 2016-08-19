a18392a90fb200842b8d26d1d1e4f807321c0002
igeligel <kevinigeligeligel@gmail.com>
2016-08-19 23:54:28 +0200

    feat(model)
    add property to the skadi trade offer

--- src/skadisteam.trade/Models/TradeOffer/SkadiTradeOffer.cs
+++ src/skadisteam.trade/Models/TradeOffer/SkadiTradeOffer.cs
@@ -2,6 +2,7 @@ namespace skadisteam.trade.Models.TradeOffer
 {
     public class SkadiTradeOffer
     {
+        public int Id { get; set; }
         public SkadiTradeOfferProfile MyProfile { get; set; }
         
         public SkadiTradeOfferPartner Partner { get; set; }
