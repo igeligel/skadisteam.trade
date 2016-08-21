00604f7ae7f0394333c2246c2dab4f77b251c50c
igeligel <kevinigeligeligel@gmail.com>
2016-08-22 01:02:49 +0200

    feat(constants)
    add new key for http headers: Origin and X-Requested-With

--- src/skadisteam.trade/Constants/HttpHeaderKeys.cs
+++ src/skadisteam.trade/Constants/HttpHeaderKeys.cs
@@ -6,9 +6,11 @@ namespace skadisteam.trade.Constants
         internal const string AcceptEncoding = "Accept-Encoding";
         internal const string AcceptLanguage = "Accept-Language";
         internal const string CacheControl = "Cache-Control";
+        internal const string Origin = "Origin";
         internal const string Pragma = "Pragma";
         internal const string Referer = "Referer";
         internal const string UpgradeInsecureRequest = "Upgrade-Insecure-Requests";
         internal const string UserAgent = "User-Agent";
+        internal const string XrequestedWith = "X-Requested-With";
     }
 }
