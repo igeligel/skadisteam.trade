dc14524f2af7903072eea894fd606c18a5ad091c
igeligel <kevinigeligeligel@gmail.com>
2016-08-22 01:07:44 +0200

    feat(factories)
    add new factory for creating url's which are used in the library

---
+++ src/skadisteam.trade/Factories/UrlFactory.cs
@@ -0,0 +1,33 @@
+using skadisteam.trade.Constants;
+
+namespace skadisteam.trade.Factories
+{
+    internal class UrlFactory
+    {
+        internal static string SteamCommunityHome(long steamCommunityId)
+        {
+            return Urls.SteamCommunityBase + "/profiles/" + steamCommunityId +
+                   "/home";
+        }
+
+        internal static string TradeOfferPage(long steamCommunityId)
+        {
+            return Urls.SteamCommunityBase + "/profiles/" + steamCommunityId +
+                   "/tradeoffers/";
+        }
+
+        internal static string TradeOffer(int id)
+        {
+            return Urls.SteamCommunityBaseSecured + "/tradeoffer/" + id + "/";
+        }
+
+        internal static string TradeOfferExecutrion(long partnerCommunityId,
+            string tradeOfferToken)
+        {
+            return Urls.SteamCommunityBaseSecured + "/tradeoffer/new/?partner=" +
+                   (partnerCommunityId - SteamIds.SteamCommunityBaseId) +
+                   "&token=" +
+                   tradeOfferToken;
+        }
+    }
+}
