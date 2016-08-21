d76d50b8d67a1b1795de1e2432d95b61c07a9018
igeligel <kevinigeligeligel@gmail.com>
2016-08-22 01:03:09 +0200

    feat(constants)
    add new http header values

--- src/skadisteam.trade/Constants/HttpHeaderValues.cs
+++ src/skadisteam.trade/Constants/HttpHeaderValues.cs
@@ -2,11 +2,24 @@ namespace skadisteam.trade.Constants
 {
     internal static class HttpHeaderValues
     {
+        internal const string AcceptAll = "*/*";
         internal const string AcceptHtmlImagesAndXml = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
         internal const string AcceptLanguageEnglish = "en-US;q=0.6,en;q=0.4,it;q=0.2";
-        internal const string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36";
+        internal const string AcceptLanguageGermanEnglish = "de-DE,en-US;q=0.8";
+        internal const string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2743.116 Safari/537.36";
         internal const string GzipDeflateOrSdch = "gzip, deflate, sdch";
+        internal const string GzipDeflateOrBr = "gzip, deflate, br";
+        internal const string GzipDeflateSdchOrBr = "gzip, deflate, sdch, br";
+
+        internal const string MobileUserAgent =
+            "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 4 Build/LMY48T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/46.0.2490.76 Mobile Safari/537.36";
         internal const string NoCache = "no-cache";
-        
+        internal const string One = "1";
+
+        internal const string RequestedWithAndroidApp =
+            "com.valvesoftware.android.steam.community";
+
+        internal const string RequestedWithXmlHttpRequest = "XMLHttpRequest";
+
     }
 }
