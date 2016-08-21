c555312d0dd35572b05702058c37af2a5dd77819
igeligel <kevinigeligeligel@gmail.com>
2016-08-22 01:03:51 +0200

    feat(constants)
    add new uri for secured steam community base url

--- src/skadisteam.trade/Constants/Uris.cs
+++ src/skadisteam.trade/Constants/Uris.cs
@@ -5,5 +5,8 @@ namespace skadisteam.trade.Constants
     internal static class Uris
     {
         internal static Uri SteamCommunityBase = new Uri(Urls.SteamCommunityBase);
+
+        internal static Uri SteamCommunityBaseSecured =
+            new Uri(Urls.SteamCommunityBaseSecured);
     }
 }
