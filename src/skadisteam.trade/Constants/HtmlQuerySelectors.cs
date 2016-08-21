1a058e61e9a1c0a09a209591128631b46b3c0761
igeligel <kevinigeligeligel@gmail.com>
2016-08-22 01:02:15 +0200

    feat(contants)
    add new const for finding tradeoffers

--- src/skadisteam.trade/Constants/HtmlQuerySelectors.cs
+++ src/skadisteam.trade/Constants/HtmlQuerySelectors.cs
@@ -3,6 +3,7 @@ namespace skadisteam.trade.Constants
     internal static class HtmlQuerySelectors
     {
         internal const string PlayerAvatarClass = "." + HtmlClasses.PlayerAvatar;
+        internal const string TradeOffer = "div.tradeoffer";
         internal const string TradeofferHeader = ".tradeoffer_header";
         internal const string TradeOfferItemList = ".tradeoffer_item_list";
     }
