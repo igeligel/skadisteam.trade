d7bac49850a5f92b4f2cb45cd08ae23835bf4643
igeligel <kevinigeligeligel@gmail.com>
2016-08-22 01:05:16 +0200

    feat(constants)
    add constant for steam community base url

--- src/skadisteam.trade/Constants/Urls.cs
+++ src/skadisteam.trade/Constants/Urls.cs
@@ -4,5 +4,8 @@ namespace skadisteam.trade.Constants
     internal static class Urls
     {
         internal static string SteamCommunityBase = "http://steamcommunity.com";
+
+        internal static string SteamCommunityBaseSecured =
+            "https://steamcommunity.com";
     }
 }
