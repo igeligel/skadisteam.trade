61ea40863d5e1240a797b38eaec08abe89f95630
igeligel <kevinigeligeligel@gmail.com>
2016-12-22 00:14:17 +0100

    feat(constants)
    add new regex patterns

--- src/skadisteam.trade/Constants/RegexPatterns.cs
+++ src/skadisteam.trade/Constants/RegexPatterns.cs
@@ -3,10 +3,13 @@ namespace skadisteam.trade.Constants
     internal static class RegexPatterns
     {
         internal const string ClassInfo = "classinfo";
+        internal const string ClosedBracket = ")";
         internal const string CommunityId = "javascript\\SReportTradeScam\\S '";
         internal const string NewLine = "\n";
         internal const string OfferExpireson = "Offer expires on ";
         internal const string SingleQuote = "'";
+        internal const string SteamErrorFirstPart =
+            "There was an error accepting this trade offer.  Please try again later. (";
         internal const string Tab = "\t";
         internal const string TradeOfferIdUnderscore = "tradeofferid_";
     }
