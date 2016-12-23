bd07d7ed53175282f066a2ad828fe7a20133a286
Kevin Peters <git.igeligel@googlemail.com>
2016-12-23 23:34:12 +0100

    feat(exceptions)
    add exception for when the trade offer which is accessed is not valid
    anymore

---
+++ src/skadisteam.trade/Exceptions/OfferNotValidException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace skadisteam.trade.Exceptions
+{
+    public class OfferNotValidException : Exception
+    {
+        public OfferNotValidException()
+        {
+
+        }
+
+        public OfferNotValidException(string message)
+        : base(message)
+        {
+        }
+
+        public OfferNotValidException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
