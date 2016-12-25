0c1d5f476590a372536628224698617da6ea2855
Kevin Peters <git.igeligel@googlemail.com>
2016-12-25 13:39:53 +0100

    feat(constants)
    add class which holds descriptions of exceptions

---
+++ src/skadisteam.trade/Constants/ExceptionDescriptions.cs
@@ -0,0 +1,9 @@
+﻿namespace skadisteam.trade.Constants
+{
+    internal static class ExceptionDescriptions
+    {
+        internal const string DeviceIdAndIdentitySecretNotSet = "You have not set the device id and the identity secret which is required for this functionality. To fix this please use a constructor of this class where the device id and the identity secret is required as parameter.";
+        internal const string DeviceIdNotSet = "";
+        internal const string IdentitySecretNotSet = "";
+    }
+}
