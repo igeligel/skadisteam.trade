9bf471df48edbf3135d5c3f0a1712f1ad8d0a4a0
Kevin Peters <git.igeligel@googlemail.com>
2016-12-25 13:40:25 +0100

    feat(validator)
    validator for the device id and identity secret which needs to be set
    for some methods of the client

---
+++ src/skadisteam.trade/Validator/DeviceIdIdentitySecretValidator.cs
@@ -0,0 +1,24 @@
+﻿using skadisteam.trade.Constants;
+using System;
+
+namespace skadisteam.trade.Validator
+{
+    internal static class DeviceIdIdentitySecretValidator
+    {
+        internal static void Validate(string deviceId, string identitySecret)
+        {
+            if (string.IsNullOrEmpty(deviceId) || string.IsNullOrEmpty(identitySecret))
+            {
+                throw new ArgumentException(ExceptionDescriptions.DeviceIdAndIdentitySecretNotSet);
+            }
+            if (string.IsNullOrEmpty(deviceId))
+            {
+                throw new ArgumentException(ExceptionDescriptions.DeviceIdNotSet);
+            }
+            if (string.IsNullOrEmpty(identitySecret))
+            {
+                throw new ArgumentException(ExceptionDescriptions.IdentitySecretNotSet);
+            }
+        }
+    }
+}
