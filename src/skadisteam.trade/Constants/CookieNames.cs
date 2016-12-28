a0a8bfe05457f72fe7432990bda07508e14cabd0
igeligel <kevinigeligeligel@gmail.com>
2016-12-29 00:51:08 +0100

    feat(constants)
    add names and values of cookies as constants

---
+++ src/skadisteam.trade/Constants/CookieNames.cs
@@ -0,0 +1,11 @@
+namespace skadisteam.trade.Constants
+{
+    internal static class CookieNames
+    {
+        internal const string Dob = "dob";
+        internal const string MobileClient = "mobileClient";
+        internal const string MobileClientVersion = "mobileClientVersion";
+        internal const string SteamId = "steamid";
+        internal const string WebTradeEligibility = "webTradeEligibility";
+    }
+}
---
+++ src/skadisteam.trade/Constants/CookieValues.cs
@@ -0,0 +1,10 @@
+namespace skadisteam.trade.Constants
+{
+    internal static class CookieValues
+    {
+        internal const string MobileClientAndroid = "android";
+        internal const string MobileClientVersion = "0 (2.1.3)";
+        internal const string WebTradeEligibility =
+            "%7B%22allowed%22%3A1%2C%22allowed_at_time%22%3A0%2C%22steamguard_required_days%22%3A15%2C%22sales_this_year%22%3A1182%2C%22max_sales_per_year%22%3A-1%2C%22forms_requested%22%3A0%2C%22new_device_cooldown_days%22%3A7%7D";
+    }
+}
