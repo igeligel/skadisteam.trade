f4b13ee71d18f47f1149b5323e7fb305117289f3
igeligel <kevinigeligeligel@gmail.com>
2016-12-29 15:57:12 +0100

    feat(constants)
    add exception descriptions for exceptions when devide id or identity
    secret is not set

--- src/skadisteam.trade/Constants/ExceptionDescriptions.cs
+++ src/skadisteam.trade/Constants/ExceptionDescriptions.cs
@@ -1,9 +1,14 @@
-﻿namespace skadisteam.trade.Constants
+namespace skadisteam.trade.Constants
 {
     internal static class ExceptionDescriptions
     {
-        internal const string DeviceIdAndIdentitySecretNotSet = "You have not set the device id and the identity secret which is required for this functionality. To fix this please use a constructor of this class where the device id and the identity secret is required as parameter.";
-        internal const string DeviceIdNotSet = "";
-        internal const string IdentitySecretNotSet = "";
+        internal const string DeviceIdAndIdentitySecretNotSet =
+            "You have not set the device id and the identity secret which is required for this functionality. To fix this please use a constructor of this class where the device id and the identity secret is required as parameter.";
+
+        internal const string DeviceIdNotSet =
+            "You have not set your Device Id which is needed for this functionality. You should call the constructor of the client where you can set the device id.";
+
+        internal const string IdentitySecretNotSet =
+            "You have not set your identity secret which is needed for this functionality. You should call the constructor of the client where you can set the identity secret.";
     }
 }
